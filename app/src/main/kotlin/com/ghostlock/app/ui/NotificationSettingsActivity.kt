74daed815b7574b3117aecf87f82586a37916434
Basirul Akhlak Borno <basirulakhlak@gmail.com>
2026-09-20 00:31:23 +0600

    Fix missing dp import in notification settings.

--- app/src/main/kotlin/com/ghostlock/app/ui/NotificationSettingsActivity.kt
+++ app/src/main/kotlin/com/ghostlock/app/ui/NotificationSettingsActivity.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.ghostlock.app.R
 import com.ghostlock.app.data.BootAutoRootPreferences
